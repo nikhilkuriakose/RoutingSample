a6a7e5e37b101d4305145acf507e04b479c9aaf8
nikhi <nikhi@DESKTOP-6FF0Q4P>
2022-12-04 13:52:26 -0500

    Add project files.

---
+++ BL/TimeConverter.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class TimeConverter : ITimeConverter
+    {
+        public Time ConvertTime(Time fromTime, string timeZone)
+        {
+            try
+            {
+                DateTime timeToConvert = DateTime.Parse(fromTime.LocalTime);
+                return new Time
+                {
+                    LocalTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(timeToConvert, fromTime.TimeZone, timeZone).ToString(),
+                    TimeZone = timeZone
+                };
+            }
+            catch
+            {
+                return new Time
+                {
+                    LocalTime = "Error",
+                    TimeZone = "Error"
+                };
+            }
+        }
+    }
+}
---
+++ RoutingSample/Controllers/TimeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using BL;
+
+namespace RoutingSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly ITimeConverter _timeConvertor;
+        public TimeController(ITimeConverter timeConverter)
+        {
+            _timeConvertor = timeConverter;
+        }
+        [HttpPost]
+        [Route("[Action]/{toTimeZone}")]
+        public IActionResult ConvertTime(Time time, string toTimeZone)
+        {
+            if (time == null || toTimeZone == null)
+                return BadRequest("time and toTimeZone cannot be null");
+            return Ok(_timeConvertor.ConvertTime(time, toTimeZone));
+        }
+        [HttpPost]
+        [Route("[Action]/{fromTime}/{fromTimeZone}/{toTimeZone}")]
+        public IActionResult ConvertTime(string fromTime, string fromTimeZone, string toTimeZone)
+        {
+            if (fromTime == null || fromTimeZone == null || toTimeZone == null)
+                return BadRequest();
+            Time time = new Time
+            {
+                LocalTime = fromTime,
+                TimeZone = fromTimeZone
+            };
+            return Ok(_timeConvertor.ConvertTime(time, toTimeZone));
+        }
+        [HttpPost]
+        [Route("[Action]")]
+        public IActionResult ConvertTimeQuery(string fromTime, string fromTimeZone, string toTimeZone)
+        {
+            if (fromTime == null || fromTimeZone == null || toTimeZone == null)
+                return BadRequest();
+            Time time = new Time
+            {
+                LocalTime = fromTime,
+                TimeZone = fromTimeZone
+            };
+            return Ok(_timeConvertor.ConvertTime(time, toTimeZone));
+        }
+    }
+}
